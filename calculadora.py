71d75323c7b3658a04b89ebc27c7017efbdf30bc
Lucas Rabelo <lucasgagrabelo@outlook.com>
2023-12-02 18:46:03 -0300

    convertendo decimal para real

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
-# Receber valores do usuario
+# receber valores do usuario
 def value():
     while True:
-        entrada = input("Digite um número(ou '0' para voltar ao menu inicial): ")
+        entrada = input("Digite um número (ou '0' para voltar ao menu inicial): ")
         if entrada == '0':
             return None
         entrada = entrada.replace(",", ".")
@@ -10,6 +10,12 @@ def value():
             return numero
         except ValueError:
             print("Entrada inválida. Por favor, insira um número válido.")
+
+# converter de decimal para inteiro
+def converter_decimal_para_inteiro(numero):
+    if numero.is_integer() or int(numero) == numero:
+        return int(numero)
+    return numero
 
 # Escolha o tipo de operacao
 while True:
@@ -28,18 +34,19 @@ while True:
         value2 = value()
         if value1 is not None and value2 is not None:
             resultado = value1 + value2
-            print("Resultado: ", resultado)
+            resultado = converter_decimal_para_inteiro(resultado)
+            print(f"Resultado: {resultado:,}")
     elif escolha == '2':
         value1 = value()
         value2 = value()
         if value1 is not None and value2 is not None:
             if value1 >= value2:
-                resultado = int(value1) - int(value2)
-                print("Resultado: ", resultado)
+                resultado = value1 - value2
+                resultado = converter_decimal_para_inteiro(resultado)
+                print(f"Resultado: {resultado:,}")
             else:
                 print("O primeiro valor deve ser maior que o segundo")
         else:
             print("Valores inválidos para subtração.")
     else:
         print("Opção inválida. Escolha 1, 2 ou 0 para sair.")
-        
